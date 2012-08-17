d480ca0a62da6f2da12661af25cbf3db6566a1d1
kamalasaurus <kamalasaurus@gmail.com>
2012-08-16 23:33:34 -0700

    Initial Commit

---
+++ Swatchr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Swatchr
+//
+//  Created by Kamal Radharamanan on 8/16/12.
+//  Copyright (c) 2012 Kamal Radharamanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Swatchr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Swatchr
+//
+//  Created by Kamal Radharamanan on 8/16/12.
+//  Copyright (c) 2012 Kamal Radharamanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
